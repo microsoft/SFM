bcb1351bc6490d4a98cae74e7eead30a028fae00
Shufang Xie <shufxi@microsoft.com>
2025-06-20 13:01:22 +0800

    Add example notebooks (#4)
    Add example notebooks

--- NatureLM/hello.py
+++
@@ -1,6 +0,0 @@
-from transformers import pipeline, set_seed
-generator = pipeline('text-generation', model='gpt2')
-set_seed(42)
-ret = generator("Nature Language Model is")
-
-print(ret[0]['generated_text'])
